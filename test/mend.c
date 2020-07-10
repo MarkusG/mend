66c7ca4fea17f34af01737a73b0d45fe167defc8
Mark Gross <mark.gross2001@gmail.com>
2020-07-09 17:39:20 -0700

    Fix mend_init() and mend_cleanup() unit tests

--- test/mend.c
+++ test/mend.c
@@ -3,15 +3,28 @@
 
 #include "../include/mend.h"
 
-int main(int argc, char *argv[])
-{
+int mend_init_test() {
 	if (mend_init("postgresql://postgres@db/mend")) {
 		printf("%s\n", mend_error());
 		return 1;
 	}
 
-	if (strcmp(argv[1], "mend_cleanup"))
-		return mend_cleanup();
-
 	return 0;
+}
+
+int mend_cleanup_test() {
+	return mend_cleanup();
+}
+
+int main(int argc, char *argv[])
+{
+	const char *cmd = argv[1];
+	if (strcmp(cmd, "mend_init") == 0)
+		return mend_init_test();
+	else if (strcmp(cmd, "mend_cleanup") == 0)
+		return mend_cleanup_test();
+	else {
+		printf("no command specified (somebody wrote a test wrong)\n");
+		return 1;
+	}
 }
