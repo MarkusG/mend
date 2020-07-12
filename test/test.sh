528d49e29de939f6dc0b1878e2ede947ed50658f
Mark Gross <mark.gross2001@gmail.com>
2020-07-11 19:20:22 -0700

    tests: add tables to right database

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 psql -U postgres postgresql://db -c 'CREATE DATABASE mend'
-psql -U postgres postgresql://db -f sql/tables.sql
+psql -U postgres postgresql://db/mend -f sql/tables.sql
 ninja -C .build test
