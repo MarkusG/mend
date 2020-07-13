0f6bbded8ad61de53a189cdf62226336a4b6913e
Mark Gross <mark.gross2001@gmail.com>
2020-07-13 15:36:35 -0700

    mend: add list_aliases.c
    Forgot this oops

---
+++ src/command/list_aliases.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+#include "command.h"
+#include "../utils.h"
+#include "../../include/mend.h"
+
+int list_aliases(options *options) {
+	const char *id = options->identifiers[1];
+	if (!id) {
+		fprintf(stderr, ERR "no identifier specified\n");
+		return 1;
+	}
+
+	mend_id_kind kind;
+	if (is_uuid(id))
+		kind = MEND_UUID;
+	else
+		kind = MEND_NAME;
+
+	const mend_entity *entity = mend_get_entity(id, kind);
+	const mend_alias **aliases = mend_get_aliases(entity);
+	if (!aliases) {
+		fprintf(stderr, ERR "%s\n", mend_error());
+		return 1;
+	}
+	print_entity(entity, options);
+	putchar('\n');
+	int i = 0;
+	const mend_alias *alias;
+	while ((alias = aliases[i])) {
+		printf("%s\n", mend_alias_value(alias));
+		mend_free_alias(alias);
+		++i;
+	}
+	free((void*)aliases);
+	mend_free_entity(entity);
+	return 0;
+}
