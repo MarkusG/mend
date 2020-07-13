9b53d8ca37672dc00f66dec265e54c202cd0c572
Mark Gross <mark.gross2001@gmail.com>
2020-07-13 08:53:13 -0700

    tests: follow capitalization rules in tables.sql

--- test/tables.sql
+++ test/tables.sql
@@ -1,5 +1,5 @@
-INSERT INTO ENTITY (uid, name, created) VALUES (UUID 'a09d03f1-9e17-4127-b22c-08fbe65ef07f', 'Foo', TIMESTAMP WITHOUT TIME ZONE '2001-09-28 23:00');
-INSERT INTO ENTITY (uid, name, created) VALUES (UUID 'bc05cd9e-098a-42c6-a92f-21f4529a9f89', 'Bar', TIMESTAMP WITHOUT TIME ZONE '2001-10-28 23:00');
-INSERT INTO ENTITY (uid, name, created) VALUES (UUID '17e3f02a-8e2d-4a66-abca-d1082a9f9508', 'Baz', TIMESTAMP WITHOUT TIME ZONE '2001-11-28 23:00');
-INSERT INTO ENTITY (uid, name) VALUES (UUID '70358375-01b7-4f87-b477-3711fbacedc6', 'to_be_removed');
-INSERT INTO ENTITY (uid, name) VALUES (UUID 'ddbfba06-b10c-4344-a343-ccc63321d435', 'to_be_removed_by_uuid');
+INSERT INTO entity (uid, name, created) VALUES (UUID 'a09d03f1-9e17-4127-b22c-08fbe65ef07f', 'Foo', TIMESTAMP WITHOUT TIME ZONE '2001-09-28 23:00');
+INSERT INTO entity (uid, name, created) VALUES (UUID 'bc05cd9e-098a-42c6-a92f-21f4529a9f89', 'Bar', TIMESTAMP WITHOUT TIME ZONE '2001-10-28 23:00');
+INSERT INTO entity (uid, name, created) VALUES (UUID '17e3f02a-8e2d-4a66-abca-d1082a9f9508', 'Baz', TIMESTAMP WITHOUT TIME ZONE '2001-11-28 23:00');
+INSERT INTO entity (uid, name) VALUES (UUID '70358375-01b7-4f87-b477-3711fbacedc6', 'to_be_removed');
+INSERT INTO entity (uid, name) VALUES (UUID 'ddbfba06-b10c-4344-a343-ccc63321d435', 'to_be_removed_by_uuid');
